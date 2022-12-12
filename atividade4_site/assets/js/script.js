a1aff76c2f73be680d90c3923c736ccfe1a223c9
Igor-mt <igor.limall@outlook.com>
2022-12-12 17:32:40 -0300

    Finalização site

--- atividade4_site/assets/js/script.js
+++ atividade4_site/assets/js/script.js
@@ -30,3 +30,19 @@ const backTopBtn = document.querySelector("[data-back-to-top]");
       backTopBtn.classList.remove("active");
     }
   });
+
+  function checkScroll(){
+    var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
+
+    if($(window).scrollTop() > startY){
+        $('.navbar').addClass("scrolled");
+    }else{
+        $('.navbar').removeClass("scrolled");
+    }
+}
+
+if($('.navbar').length > 0){
+    $(window).on("scroll load resize", function(){
+        checkScroll();
+    });
+}
